907a62480115b5c670e5400c5a721977af45b944
Michael Petö <michael@petoe.me>
2017-05-08 14:52:46 +0200

    Add upload test and check for equality

--- spec/haste_spec.cr
+++ spec/haste_spec.cr
@@ -1,9 +1,16 @@
 require "./spec_helper"
+require "http/client"
 
-describe Haste do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+describe "Haste" do
+  it "Upload" do
+    uploader = Haste::Uploader.new ENV["HASTE_SERVER"]?
+    key = uploader.upload_raw "haste-client upload test."
+    puts "Got key #{key}."
+  end
+  it "Upload should eq" do
+    uploader = Haste::Uploader.new ENV["HASTE_SERVER"]?
+    key = uploader.upload_raw "haste-client upload test."
+    response = HTTP::Client.get URI.parse("https://hastebin.com/raw/#{key}")
+    response.body.should eq("haste-client upload test.")
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/haste"
+require "../src/haste/*"
